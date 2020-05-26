4969ee99fc8087cd4e4cbb5a7afadfab366de030
Fenil Desai <feniludesai@yahoo.com>
2020-05-26 07:47:04 +0100

    deploy to netlify changes
    deploy to netlify changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component{
       cured: 0,
       death: 0,
       total: 0,
-      states: []
+      states: [],
   };
 
 
@@ -24,19 +24,28 @@ export default class App extends React.Component{
   }
 
   async getData(){
-      const resStates = await Axios.get("http://covid19-india-adhikansh.herokuapp.com/states");
+      const resApi = await Axios.get("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise/");
+      const resStates = await Axios.get("https://covid19-india-adhikansh.herokuapp.com/states");
       const states = [];
       for(var i = 0; i < resStates.data.state.length; i++){
           states.push(resStates.data.state[i].name);
       }
-      const res = await Axios.get(`http://covid19-india-adhikansh.herokuapp.com/state/${states[0]}`);
+      //const res = await Axios.get(`https://covid19-india-adhikansh.herokuapp.com/state/${states[0]}`);
+
+    let res = resApi.data.data.statewise.filter(d => 
+        {return d.state.match(states[0])});
+
       //const countries = Object.keys(resCountries.data.countries);
       this.setState({
-        confirmed:res.data.data[0].confirmed,
-        cured:res.data.data[0].cured,
-        death:res.data.data[0].death,
-        total:res.data.data[0].total,
-          states
+        confirmed:res[0].confirmed,
+        cured:res[0].recovered,
+        death:res[0].deaths,
+        total:res[0].active,
+          states,
+          indiaConfirmed: resApi.data.data.total.confirmed,
+          indiaRecovered: resApi.data.data.total.recovered,
+          indiaDeaths: resApi.data.data.total.deaths,
+          indiaActive: resApi.data.data.total.active
       });
   }
 
@@ -44,14 +53,18 @@ export default class App extends React.Component{
       if(e.target.value === null) {
           return this.getData();     
       }
+
+      let stateValue = e.target.value;
       
       try{
-      const res = await Axios.get(`http://covid19-india-adhikansh.herokuapp.com/state/${e.target.value}`);
+      const resApi = await Axios.get("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise/");
+      let res = resApi.data.data.statewise.filter(d => 
+        {return d.state.match(stateValue)});
       this.setState({
-          confirmed:res.data.data[0].confirmed,
-          cured:res.data.data[0].cured,
-          death:res.data.data[0].death,
-          total:res.data.data[0].total,
+        confirmed:res[0].confirmed,
+        cured:res[0].recovered,
+        death:res[0].deaths,
+        total:res[0].active,
       });
   }
   catch(err){
@@ -75,14 +88,30 @@ export default class App extends React.Component{
   render(){
       return (
       <div className="container">
-          <h1>Covid-19 Indian States Tracker</h1>
+          <h1>Covid-19 India Tracker</h1>
+          <div className="flex">
+          <div className="box confirmed">
+              <h3>Confirmed Cases</h3>
+              <h4>{this.state.indiaConfirmed}</h4>
+          </div>
+          <div className="box recovered">
+              <h3>Recovered Cases</h3>
+              <h4>{this.state.indiaRecovered}</h4>
+          </div>
+          <div className="box deaths">
+              <h3>Deaths</h3>
+              <h4>{this.state.indiaDeaths}</h4>
+          </div>
+          <div className="box total">
+              <h3>Total Active</h3>
+              <h4>{this.state.indiaActive}</h4>
+          </div>
+          </div>
 
+          <h2>Select State:</h2>
           <select className="dropdown" onChange={this.getStatesData}>
               {this.renderStatesOptions()}
           </select>
-
-
-
 
           <div className="flex">
 
